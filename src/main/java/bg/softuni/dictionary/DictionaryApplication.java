bc759930e5bcc73762ca1827175f1172d63b18a2
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-22 10:17:12 +0200

    Dictionary

---
+++ src/main/java/bg/softuni/dictionary/DictionaryApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryApplication.class, args);
+    }
+
+}
