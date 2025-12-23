cc2d458adb194f78a476289a42b24282fcca405a
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-23 10:17:45 +0200

    Dictionary

---
+++ src/test/java/bg/softuni/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
