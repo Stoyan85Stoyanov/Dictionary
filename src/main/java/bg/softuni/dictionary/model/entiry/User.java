3b2c98b9e55dcb0fd9b0b04f05dd57f61ab36a90
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-27 12:19:38 +0200

    Dictionary

---
+++ src/main/java/bg/softuni/dictionary/model/entiry/User.java
@@ -0,0 +1,30 @@
+package bg.softuni.dictionary.model.entiry;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "addedBy", fetch = FetchType.EAGER)
+    private Set<Word> addedWords;
+}
