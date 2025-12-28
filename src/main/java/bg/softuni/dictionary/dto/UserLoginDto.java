4a1118682e158a78f4413238427fdadbd264def4
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-28 22:44:32 +0200

    Dictionary

---
+++ src/main/java/bg/softuni/dictionary/dto/UserLoginDto.java
@@ -0,0 +1,21 @@
+package bg.softuni.dictionary.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDto {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String password;
+}
